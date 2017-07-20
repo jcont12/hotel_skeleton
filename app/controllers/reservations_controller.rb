6ba88dfc9cab5987d020a378c6ed28ce07e5eb2f
Jorge Contreras & Kala Wetzler <pair+jcont12.kala-lee@devbootcamp.com>
2017-07-19 19:30:14 -0700

    Nested form working

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,6 +4,28 @@ class ReservationsController < ApplicationController
   end
 
   def create
-    @reservations.create(params[:reservation])
+    p reservation_params
+    @guest = Guest.create(guests_params[:guests])
+    p '*' * 50
+    p @guest
+    @reservation = Reservation.new(reservation_params)
+    p '*' * 50
+    p @reservation
+    @reservation.guest_id = @guest.id
+    @reservation.save!
+    render 'show'
   end
+
+  def show
+  end
+
+  private
+  def reservation_params
+    params.require(:reservation).permit(:quantity, :check_in, :check_out, :comments)
+  end
+
+  def guests_params
+    params.require(:reservation).permit(guests: [:first_name, :last_name, :email])
+  end
+
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,7 @@
 class Reservation < ApplicationRecord
   belongs_to :guest
   has_many :reservation_rooms
+  accepts_nested_attributes_for :guest
 
   validates_presence_of :guest, :quantity, :check_in, :check_out
 end
